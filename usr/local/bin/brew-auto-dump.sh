671218fe57c0d95745929a1544e6320236371906
Martin Juul <code@juul.xyz>
2019-09-09 16:36:25 +0200

    improve brew-auto-dump

--- usr/local/bin/brew-auto-dump.sh
+++ usr/local/bin/brew-auto-dump.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 
-WORKDIR=/Volumes/Dev/personal/bootstrap-macos
+set -xe
 
-cd "${WORKDIR}"
+WORKDIR=/Volumes/Dev/personal/bootstrap-macos
+DUMP_DATE=$(date "+%Y-%m-%d %H:%M:%S")
+
+cd "${WORKDIR}" || (echo "${WORKDIR} does not exist" && exit 1)
 
 brew bundle dump --force
 
 git add "${WORKDIR}/Brewfile"
 
-git commit -m "[Brewfile]: dump `date`"
+git commit -m "[Brewfile]: dump bundle - ${DUMP_DATE}"
 
 git push origin master
